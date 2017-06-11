18021c1f5decd4c1b205e3dcb47dbf68fd13a69f
Romario <romario.curcio@corp.globo.com>
2017-06-11 17:12:03 -0300

    adding validations and anotations for password encrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ApplicationRecord
+
+	has_secure_password
+	validates :password, presence: true, length: {minimum: 6}
+  	VALID_EMAIL_FORMAT= /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+  	validates :email, presence: true, length: {maximum: 260}, format: { with: VALID_EMAIL_FORMAT}, uniqueness: {case_sensitive: false}
+  	before_save { self.email = email.downcase }
+
 	has_many :interactions
     has_many :comments, through: :interactions
     has_many :acceptances, through: :interactions
     
     validates :name, presence:true
-    validates :email, presence:true
 end
