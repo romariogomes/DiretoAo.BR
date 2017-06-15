8bb60185abad1a35227884e79dbe393bf198d79e
Romario <romario.curcio@corp.globo.com>
2017-06-15 18:42:14 -0300

    adding helper to create a new interaction

--- app/helpers/law_projects_helper.rb
+++ app/helpers/law_projects_helper.rb
@@ -3,4 +3,13 @@ module LawProjectsHelper
 	def load_politicians
 		@politicians = Politician.all
 	end
+
+	def create_interaction
+		
+		@interaction = Interaction.new
+		@interaction.user = current_user if logged_in?
+		@interaction.law_project_id = params['comment']['law_project_id']
+		
+		@interaction.save
+	end
 end
