3f2942b1e5f341bd93351df7eb61e175a04db1bb
Romario <romario.curcio@corp.globo.com>
2017-06-16 19:49:52 -0300

    adding helper to create interaction for acceptance

--- app/helpers/law_projects_helper.rb
+++ app/helpers/law_projects_helper.rb
@@ -13,6 +13,15 @@ module LawProjectsHelper
 		@interaction.save
 	end
 
+	def create_interaction_acceptance
+		
+		@interaction = Interaction.new
+		@interaction.user = current_user if logged_in?
+		@interaction.law_project_id = params['acceptance']['law_project_id']
+		
+		@interaction.save
+	end
+
 	def load_all_comments(law_project_id)
 		@all_comments = Interaction.where(law_project_id: law_project_id)
 	end
