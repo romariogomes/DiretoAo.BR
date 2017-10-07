e7a899d7559f081ed916cbc3269ff9bd2558d318
Romario <romario.curcio@corp.globo.com>
2017-10-06 21:01:10 -0300

    starting file implementention

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,6 +1,24 @@
 class RankingController < ApplicationController
 
 	def index
+		loadRanking
+	end
+
+	def loadRanking
+		
+		interactions = Interaction.where(user_id: current_user.id)
+		acceptancesList = Array.new
+		
+		interactions.each do |i|
+	        
+        	if (!i.acceptance.nil?)
+          		acceptancesList.push(i)
+          	end
+        end
+
+        writeFile(acceptancesList)
+      
+
 	end
 
 	def data
@@ -9,5 +27,35 @@ class RankingController < ApplicationController
 	       render :json => [1,2,3,4,5]
 	        }
 	     end
+    end
+
+    def writeFile(acceptancesList)
+
+    	
+    	file = File.new "public/aeo.csv", "w"
+
+    	if ( !(acceptancesList.empty?) &&  !(acceptancesList.nil?) )
+
+    		acceptancesList.each do |a|
+			    require 'pry'
+			    binding.pry
+		    	file.puts a.law_project.politicians.first.party, a.law_project.politicians.first.name, "\n"
+		        	
+		    end
+
+	    # 	file = "aeo.txt"
+		  	# File.open(file, "w") do |csv|
+				  	
+		   #  	acceptancesList.each do |a|
+			        
+		   #  		csv << [a.law_project.politicians.first.party, a.law_project.politicians.first.name, "\n"]       
+		        	
+		   #      end
+	  		# end	
+	    end
+    	
+    	
+
+    	
     end
 end
